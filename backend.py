deae9e50c46b95c3638d1061d7ac5233f8e81199
Declan <declan.jp.byrne@gmail.com>
2021-05-13 20:37:14 -0700

    fixed list concatenation issue

--- backend.py
+++ backend.py
@@ -146,7 +146,8 @@ class User ():
     self.to_recommend = self.to_recommend[RecBatches:]
   def update_rec_list(self, matrix):
     if len(self.curr_rec_list) < BoostThreshold:
-      self.curr_rec_list.append(self.to_recommend[0:BoostBatch])
+      #pdb.set_trace()
+      self.curr_rec_list = self.curr_rec_list + (self.to_recommend[0:BoostBatch])
       self.to_recommend = self.to_recommend[BoostBatch:]
     for i in range(len(self.curr_rec_list)):
       self.curr_rec_list[i][1] = getRec(self, self.curr_rec_list[i][0], matrix)
